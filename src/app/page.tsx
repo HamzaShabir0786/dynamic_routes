228c2c266129e941717ca8d365fe6e0d5affa7f4
Hamza Shabir <hamzii2205@gmail.com>
2025-01-02 05:54:16 +0500

    add some data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1 className="text-white font-bold text-center  mt-24 text-[2rem]">
+        Hello World!
+      </h1>
+      <Link
+        href={"/products"}
+        className="text-white font-bold ml-[49rem]  mt-24 text-[2rem]"
+      >
+        {" "}
+        Click me{" "}
+      </Link>
     </>
   );
 }
--- src/app/products/[price]/[brand]/page.tsx
+++ src/app/products/[price]/[brand]/page.tsx
@@ -7,7 +7,7 @@ export default function Products({
   let imageProduct: string = params.brand.toLowerCase();
   return (
     <>
-      <div className="bg-pink-300 w-[30%] h-[50vh] m-auto flex flex-col items-center mt-20 rounded-3xl">
+      <div className="bg-gray-600 w-[30%] h-[50vh] m-auto flex flex-col items-center justify-around mt-20 rounded-3xl">
         <Image
           src={`/images/${imageProduct}.jpg`}
           width={200}
@@ -15,7 +15,7 @@ export default function Products({
           height={100}
           className="mt-5 rounded-2xl"
         ></Image>
-        <div className="bg-purple-400 flex gap-8 font-bold text-[1.4rem] mt-6 p-3 rounded-lg">
+        <div className="bg-gray-400 flex gap-8 font-bold text-[1.4rem] mt-6 p-3 rounded-lg">
           <h1>Price: {params.price}</h1>
           <h2>Product: {params.brand} </h2>
         </div>
